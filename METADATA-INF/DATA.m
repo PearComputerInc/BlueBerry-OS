81bfebb61fa425c96782595e890672c2b84a62f7
PearComputerInc <80929123+PearComputerInc@users.noreply.github.com>
2021-05-13 19:44:51 -0400

    Create DATA.m

---
+++ METADATA-INF/DATA.m
@@ -0,0 +1,8 @@
+executablename = "BlueBerry OS"
+executabletype = "os-replacer"
+hexexecutableid = "352b313230"
+binaryexecutableid = "11010100101011001100010011001000110000"
+factorialexecutableid = "5+120"
+  executable = 1
+  startonboot = 1
+  system = "sbgs"
